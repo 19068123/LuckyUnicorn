5f998fe09566419feb29c4418f1be84429b986eb
19068123 <129132732+19068123@users.noreply.github.com>
2023-04-06 11:22:59 +1200

    Add files via upload

---
+++ 01.yes_no_v1.py
@@ -0,0 +1,19 @@
+# Ask the user if they have played before
+show_instructions = input("Have you played"" this slay queen of a game before? ").lower()
+# If they say yes, output 'program continues'
+if show_instructions == "yes":
+    print("slayyyyyyyyy")
+
+
+elif show_instructions == "y":
+    print("slayyyyyyyy ")
+
+elif show_instructions == "no":
+    print("ew")
+
+elif show_instructions == "n":
+    print("ew")
+
+# If they say no, output 'display instructions'
+else:
+    print("Answer yes or no please")
